08ede4def9b02fca37b21f46bf75c208df2152ff
sergio.estrella <sergio.estrella@ext.soprasteria.com>
2023-06-13 09:58:31 +0200

    gracias capcom2

--- public/js/frontend.js
+++ public/js/frontend.js
@@ -66,6 +66,7 @@ let animationId
 function animate() {
   animationId = requestAnimationFrame(animate)
   c.fillStyle = 'rgba(0, 0, 0, 0)'
+  c.fillRect(0, 0, canvas.width, canvas.height)
 
   for (const id in frontEndPlayers) {
     const frontEndPlayer = frontEndPlayers[id]
